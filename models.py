270f3bfb983d9b0f13fb6b4e233f42b371160cf7
Olesia Vysotina <vysotina2002@gmail.com>
2025-02-18 22:39:35 +0100

    first commit

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, Float, Boolean, ForeignKey
+
+Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = "students"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(50))
+    group_id: Mapped["Group"] = relationship("Group", back_populates="students")
+
+
+class Subject(Base):
+    __tablename__ = "subjects"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+
+
+class Group(Base):
+    __tablename__ = "groups"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    students: Mapped[Student] = relationship("Student", back_populates="group_id")
+    subjects: Mapped[Subject] = relationship("Subject", secondary="group_subjects")
