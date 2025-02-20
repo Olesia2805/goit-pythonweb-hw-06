4d33580d2d3d20367628060359c271720dd56802
Olesia Vysotina <vysotina2002@gmail.com>
2025-02-20 23:03:22 +0100

    added Marks and modificate Subject, Group

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy import Integer, String, Float, Boolean, ForeignKey, Date
 
 Base = declarative_base()
 
@@ -17,19 +17,25 @@ class Subject(Base):
     __tablename__ = "subjects"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
+    teacher_id: Mapped["Teacher"] = relationship("Teacher", back_populates="subjects")
 
 
 class Group(Base):
     __tablename__ = "groups"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
-    students: Mapped[Student] = relationship("Student", back_populates="group_id")
-    subjects: Mapped[Subject] = relationship("Subject", secondary="group_subjects")
 
 
 class Teacher(Base):
     __tablename__ = "teachers"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
-    email: Mapped[str] = mapped_column(String(50))
-    subjects: Mapped[Subject] = relationship("Subject", back_populates="teachers")
+
+
+class Marks(Base):
+    __tablename__ = "marks"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    mark: Mapped[int] = mapped_column(Integer)
+    date: Mapped[Date] = mapped_column(Date)
+    student_id: Mapped["Student"] = relationship("Student", back_populates="marks")
+    subject_id: Mapped["Subject"] = relationship("Subject", back_populates="marks")
