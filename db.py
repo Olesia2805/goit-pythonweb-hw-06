ee4f12d9641db1d6b4313fbc9403a7637d8261d3
Olesia Vysotina <vysotina2002@gmail.com>
2025-02-22 00:04:36 +0100

    updated columns and added to DB

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-DB_URL = "postgresql+asyncpg://postgres:567234@localhost:5432/university"
+DB_URL = "postgresql://postgres:567234@localhost:5432/university"
 
 engine = create_engine(DB_URL)
 session = Session(engine)
--- models.py
+++ models.py
@@ -10,14 +10,20 @@ class Student(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(50))
-    group_id: Mapped["Group"] = relationship("Group", back_populates="students")
+    group_id: Mapped[int] = mapped_column(Integer, ForeignKey("groups.id"))
+
+    group: Mapped["Group"] = relationship("Group", back_populates="students")
+    marks: Mapped[list["Marks"]] = relationship("Marks", back_populates="students")
 
 
 class Subject(Base):
     __tablename__ = "subjects"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
-    teacher_id: Mapped["Teacher"] = relationship("Teacher", back_populates="subjects")
+    teacher_id: Mapped[int] = mapped_column(Integer, ForeignKey("teachers.id"))
+
+    teacher: Mapped["Teacher"] = relationship("Teacher", back_populates="subjects")
+    marks: Mapped[list["Marks"]] = relationship("Marks", back_populates="subject")
 
 
 class Group(Base):
@@ -25,11 +31,17 @@ class Group(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
 
+    students: Mapped[list["Student"]] = relationship("Student", back_populates="group")
+
 
 class Teacher(Base):
     __tablename__ = "teachers"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
+
+    subjects: Mapped[list["Subject"]] = relationship(
+        "Subject", back_populates="teachers"
+    )
 
 
 class Marks(Base):
@@ -37,5 +49,8 @@ class Marks(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     mark: Mapped[int] = mapped_column(Integer)
     date: Mapped[Date] = mapped_column(Date)
-    student_id: Mapped["Student"] = relationship("Student", back_populates="marks")
-    subject_id: Mapped["Subject"] = relationship("Subject", back_populates="marks")
+    student_id: Mapped[int] = mapped_column(Integer, ForeignKey("students.id"))
+    subject_id: Mapped[int] = mapped_column(Integer, ForeignKey("subjects.id"))
+
+    student: Mapped["Student"] = relationship("Student", back_populates="marks")
+    subject: Mapped["Subject"] = relationship("Subject", back_populates="marks")
